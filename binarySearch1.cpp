4a866be2d12a30e13b9c76f9c2e346d16fc37be0
Jai Bairagi <110719651+jaibairagi01@users.noreply.github.com>
2023-03-11 21:34:06 +0530

    Add files via upload

---
+++ binarySearch1.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+int firstOcc(int arr[], int n, int key)
+{
+    int s = 0, e = n - 1;
+    int mid = s + (e - s) / 2;
+    int ans = -1;
+    while (s <=e)
+    {
+        if (arr[mid] == key)
+        {
+            ans = mid;
+            e = mid - 1;
+        }
+        else if (key > arr[mid])
+        {
+            s = mid + 1;
+        }
+        else if (key < arr[mid])
+        {
+            e = mid - 1;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return ans;
+}
+
+int lastOcc(int arr[], int n, int key)
+{
+    int s = 0, e = n - 1;
+    int mid = s + (e - s) / 2;
+    int ans = -1;
+    while (s <= e)
+    {
+        if (arr[mid] == key)
+        {
+            ans = mid;
+            s = mid + 1;
+        }
+        else if (key > arr[mid])  
+        {
+            s = mid + 1;
+        }
+        else if (key < arr[mid])
+        {
+            e = mid - 1;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return ans;
+}
+int main()
+{
+    int even[5] = {1, 2, 3, 3, 5};
+
+    cout << " First Occurence of 3 is at Index " << firstOcc(even, 5, 3) << endl;
+    cout << " Last Occurence of 3 is at Index " << lastOcc(even, 5, 3) << endl;
+    return 0;
+}
